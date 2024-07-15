665376a4e810424e1255dd10c6f742c71abcd279
leomosley <leo.mosley2@gmail.com>
2024-07-15 20:23:29 +0100

    fix add padding to page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Demo } from "@/components/demo";
 
 export default function Home() {
   return (
-  <main className="flex flex-col gap-2 max-w-md mx-auto justify-center h-screen">
+  <main className="flex flex-col p-4 gap-2 max-w-md mx-auto justify-center h-screen">
     <Demo />
   </main>    
   );
